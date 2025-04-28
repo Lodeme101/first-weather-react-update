799c29bb853a9ecf8b6b00a44d04e288609ccd84
Lodeme101 <lodeme101@gmail.com>
2025-04-28 13:10:14 +0900

    Deploy debugging

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
