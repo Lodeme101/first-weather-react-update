635d6f3af33a5536e2d6a6ef18d615300302e29e
Lodeme101 <lodeme101@gmail.com>
2025-04-28 13:15:50 +0900

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -20,7 +20,7 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function (dailyForecast, index) {
+          {forecast.map((dailyForecast, index) => {
             if (index < 5) {
               return (
                 <div className="col" key={index}>
